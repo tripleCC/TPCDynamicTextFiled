364347deee18dc7b0800dd4b277cb7b405afc840
tripleCC <562327931@qq.com>
2015-08-03 21:23:53 +0800

    change the code style to swift
    change the code style to swift

--- TPCDynamicTextFiled/ViewController.swift
+++ TPCDynamicTextFiled/ViewController.swift
@@ -9,39 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var textFiled2: TPCDynamicTextFiled!
+    @IBOutlet weak var textFiled2: TPCDynamicTextFiled! {
+        didSet {
+                textFiled2.animationType = .TPCAnimationTypeLeftRight
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        textFiled2.animationType = TPCAnimationType.TPCAnimationTypeLeftRight
-        
         var textFiled3 = TPCDynamicTextFiled()
         textFiled3.normalColor = UIColor.darkGrayColor()
-        textFiled3.frame = CGRectMake(40, CGRectGetMaxY(textFiled2.frame) + 10, 200, 30)
-        textFiled3.borderStyle = UITextBorderStyle.RoundedRect
+        textFiled3.frame = CGRect(x: 40, y: CGRectGetMaxY(textFiled2.frame) + 10, width: 200, height: 30)
+        textFiled3.borderStyle = .RoundedRect
         textFiled3.placeholder = "请输入账号"
         textFiled3.font = UIFont.systemFontOfSize(14)
-        textFiled3.animationType = TPCAnimationType.TPCAnimationTypeEasyInOut
+        textFiled3.animationType = .TPCAnimationTypeEasyInOut
         self.view.addSubview(textFiled3)
         
         var textFiled4 = TPCDynamicTextFiled()
         textFiled4.normalColor = UIColor.lightGrayColor()
-        textFiled4.frame = CGRectMake(40, CGRectGetMaxY(textFiled3.frame) + 10, 200, 30)
-        textFiled4.borderStyle = UITextBorderStyle.RoundedRect
+        textFiled4.frame = CGRect(x: 40, y: CGRectGetMaxY(textFiled3.frame) + 10, width: 200, height: 30)
+        textFiled4.borderStyle = .RoundedRect
         textFiled4.placeholder = "请输入账号"
         textFiled4.font = UIFont.systemFontOfSize(14)
-        textFiled4.animationType = TPCAnimationType.TPCAnimationTypeBlowUp
+        textFiled4.animationType = .TPCAnimationTypeBlowUp
         self.view.addSubview(textFiled4)
         
         var textFiled5 = TPCDynamicTextFiled()
-        textFiled5.frame = CGRectMake(40, CGRectGetMaxY(textFiled4.frame) + 10, 200, 30)
-        textFiled5.borderStyle = UITextBorderStyle.RoundedRect
+        textFiled5.frame = CGRect(x: 40, y: CGRectGetMaxY(textFiled4.frame) + 10, width: 200, height: 30)
+        textFiled5.borderStyle = .RoundedRect
         textFiled5.placeholder = "请输入账号"
         textFiled5.font = UIFont.systemFontOfSize(14)
-        textFiled5.animationType = TPCAnimationType.TPCAnimationTypeNone
+        textFiled5.animationType = .TPCAnimationTypeNone
         textFiled5.operateWhenResignFirstResponder = { 
-            textFiled5.displayLableDoAnimationWithType(TPCAnimationType.TPCAnimationTypeLeftRight)
+            textFiled5.displayLableDoAnimationWithType(.TPCAnimationTypeLeftRight)
             textFiled5.textColor = UIColor.redColor()
         }
         self.view.addSubview(textFiled5)
